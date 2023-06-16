234538d06d6a81649f7047d617baea6f43b2a08d
Maicon Ramos <maiconpzo93@gmail.com>
2023-06-15 21:43:04 -0300

    ajuste

--- src/routes/apontamentoRoutes.js
+++ src/routes/apontamentoRoutes.js
@@ -1,13 +1,7 @@
 const apontamento = require('../controllers/apontamentosController');
 
 module.exports = (app) => {
-  app.post('/inserir-apontamento/', apontamento.deleteApontamento)
-  
-
-
-  app.get('/get-apontamento/', apontamento.deleteApontamento)
-
-
-
-  app.delete('/delete-apontamento/:id', apontamento.deleteApontamento)
+  app.post('/inserir-apontamento/', apontamento.inserirApontamentos)
+  app.get('/get-apontamento/', apontamento.getApontamento)
+  app.delete('/delete-apontamento/:id', apontamento.deleteApontamentos)
 }
